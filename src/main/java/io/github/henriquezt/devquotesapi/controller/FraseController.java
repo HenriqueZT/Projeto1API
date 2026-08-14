8033ee41df271063500f21b55d9b3daf98588b19
Henrique Zanchetta <henriqueztiozzo@gmail.com>
2026-08-14 12:21:55 -0300

    feat: adiciona endpoint para listar frases

--- src/main/java/io/github/henriquezt/devquotesapi/controller/FraseController.java
+++ src/main/java/io/github/henriquezt/devquotesapi/controller/FraseController.java
@@ -10,11 +10,20 @@ import java.util.List;
 @RestController
 public class FraseController {
 
+    private List<Frase> frases = new ArrayList<>();
+
+    public FraseController() {
+        Frase frase1 = new Frase(1, "Hora de tomar café");
+        Frase frase2 = new Frase(2, "Olá Mundo!");
+        Frase frase3 = new Frase(3, "Codar deixa careca");
+
+        frases.add(frase1);
+        frases.add(frase2);
+        frases.add(frase3);
+    }
 
     @GetMapping("/frases")
-    public Frase listarFrases() {
-        Frase frase = new Frase(1, "Hora de tomar café");
-
-        return frase;
+    public List<Frase> listarFrases() {
+        return frases;
     }
 }
