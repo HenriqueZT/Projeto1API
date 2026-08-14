78c2a0b52a1f42018f0d41c35621d98ee59fb6ab
Henrique Zanchetta <henriqueztiozzo@gmail.com>
2026-08-14 12:57:23 -0300

    feat: adiciona endpoint para buscar frase por ID

--- src/main/java/io/github/henriquezt/devquotesapi/controller/FraseController.java
+++ src/main/java/io/github/henriquezt/devquotesapi/controller/FraseController.java
@@ -2,6 +2,7 @@ package io.github.henriquezt.devquotesapi.controller;
 
 import io.github.henriquezt.devquotesapi.model.Frase;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.ArrayList;
@@ -14,12 +15,22 @@ public class FraseController {
 
     public FraseController() {
         frases.add(new Frase(1, "Hora de tomar café"));
-        frases.add(new Frase(1, "Olá Mundo!"));
-        frases.add(new Frase(1, "Codar deixa careca"));
+        frases.add(new Frase(2, "Olá Mundo!"));
+        frases.add(new Frase(3, "Codar deixa careca"));
     }
 
     @GetMapping("/frases")
     public List<Frase> listarFrases() {
         return frases;
+    }
+
+    @GetMapping("/frases/{id}")
+    public Frase buscarPorId(@PathVariable int id) {
+        for(Frase frase : frases) {
+            if(frase.getId() == id) {
+                return frase;
+            }
+        }
+        return null;
     }
 }
