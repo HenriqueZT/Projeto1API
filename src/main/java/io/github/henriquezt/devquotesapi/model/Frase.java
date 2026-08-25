e24e356bd8ea6b6340d1cff3df49c5d07e3155aa
Henrique Zanchetta <henriqueztiozzo@gmail.com>
2026-08-25 13:19:31 -0300

    refactor: simplifica Getters e Setters da entidade Frase com Lombok

--- src/main/java/io/github/henriquezt/devquotesapi/model/Frase.java
+++ src/main/java/io/github/henriquezt/devquotesapi/model/Frase.java
@@ -4,37 +4,23 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 @Entity
+@Getter
+@NoArgsConstructor
 public class Frase {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
 
+    @Setter
     private String frase;
-
-    public Frase() {
-    }
 
     public Frase(Integer id, String frase) {
         this.id = id;
         this.frase = frase;
     }
-
-    public Integer getId() {
-        return id;
-    }
-
-    public String getFrase() {
-        return frase;
-    }
-
-    public void setId(Integer id) {
-        this.id = id;
-    }
-
-    public void setFrase(String frase) {
-        this.frase = frase;
-    }
-
 }
